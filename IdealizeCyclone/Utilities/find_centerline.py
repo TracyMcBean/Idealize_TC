ee5c26360102b89c3be43266cda82f36ac7a487e
TracyMcBean <thkisz@yahoo.co.uk>
2020-03-17 19:46:15 +0100

    Added centerline caclulation for lonlat output

--- IdealizeCyclone/Utilities/find_centerline.py
+++ IdealizeCyclone/Utilities/find_centerline.py
@@ -4,6 +4,7 @@ import math
 from matplotlib import pyplot as plt 
 #from guppy import hpy; h=hpy()
 from find_clonlat import get_clonlat
+from find_lonlat import get_lonlat
 from multiprocessing import Pool
 
 ''' 
@@ -22,27 +23,52 @@ rad = dist in km / Earth radius in km
 # Variables ===================================================================
 
 r_rad = 200/6371          # Using radius of 200 km calculate the radius in radian
-lev = 75                  # Set number of levels (starting from 1, lowest 
+r_deg = 200*180/(np.pi*6371)
+#lev = 75                  # Set number of levels (starting from 1, lowest 
+lev = 90
                           # z-height is highest level)
-lev_start = 35            # Level from where the calculations should begin 
-center = np.empty([41,2]) # Array containing center coordinates for each level
-lonlat_box = {'lon_up':-0.57,'lon_down':-0.68, 'lat_up': 0.17, 'lat_down': 0.30}
+#lev_start = 35            # Level from where the calculations should begin 
+lev_start = 43
+#center = np.empty([41,2]) # Array containing center coordinates for each level
+#lonlat_box = {'lon_up':-0.57,'lon_down':-0.68, 'lat_up': 0.17, 'lat_down': 0.30}
                           # limits of quadratic region to select
                           #  include some buffer because center varies in each level
-filename = "center_fiona_p_env" # Name of file where center array should be saved
+filename = "../../Data/center_fiona_reference" # Name of file where center array should be saved
 save = True               # Should center array be saved in to "filename"
 #==============================================================================
 
 # Read in data
-pres_ds = xr.open_dataset('../../../Data/pres_data.nc')
+print('Reading data...')
+#pres_ds = xr.open_dataset('../../../Data/pres_data.nc')
+pres_ds = xr.open_dataset('../../../Data/pres_data_ref_1.nc')
 
+print('Calculate environment and extract region')
 p_env = [np.mean(pres_ds.pres[0,i].values) for i in range(lev_start-1, lev)]
 
+center = np.empty([len(pres_ds.height),2])
 # extract region of fiona 
-pres_ds = pres_ds.where(pres_ds['clon'] < lonlat_box['lon_up'], drop=True)
-pres_ds = pres_ds.where(pres_ds['clon'] > lonlat_box['lon_down'], drop=True)
-pres_ds = pres_ds.where(pres_ds['clat'] > lonlat_box['lat_up'], drop=True)
-pres_ds = pres_ds.where(pres_ds['clat'] < lonlat_box['lat_down'], drop=True)
+coord_unit='deg'
+if coord_unit == 'rad':
+   lonlat_box = {'lon_up':-0.57,'lon_down':-0.68, 'lat_up': 0.17, 'lat_down': 0.30}
+
+   pres_ds = pres_ds.where(pres_ds['clon'] < lonlat_box['lon_up'], drop=True)
+   pres_ds = pres_ds.where(pres_ds['clon'] > lonlat_box['lon_down'], drop=True)
+   pres_ds = pres_ds.where(pres_ds['clat'] > lonlat_box['lat_up'], drop=True)
+   pres_ds = pres_ds.where(pres_ds['clat'] < lonlat_box['lat_down'], drop=True)
+
+   lon = pres_ds.clon
+   lat = pres_ds.clat
+elif coord_unit =='deg':
+   lonlat_box = {'lon_up':-32.66,'lon_down':-38.96, 'lat_up': 9.74, 'lat_down': 17.19}
+
+   pres_ds = pres_ds.where(pres_ds['lon'] < lonlat_box['lon_up'], drop=True)
+   pres_ds = pres_ds.where(pres_ds['lon'] > lonlat_box['lon_down'], drop=True)
+   pres_ds = pres_ds.where(pres_ds['lat'] > lonlat_box['lat_up'], drop=True)
+   pres_ds = pres_ds.where(pres_ds['lat'] < lonlat_box['lat_down'], drop=True)
+
+   lon = pres_ds.lon
+   lat = pres_ds.lat
+
 
 #print(h.heap())
 
@@ -50,7 +76,7 @@ pres_ds = pres_ds.where(pres_ds['clat'] < lonlat_box['lat_down'], drop=True)
 show_region = False
 if show_region:
     print("Creating plot of region...")
-    plt.tripcolor(pres_ds.clon, pres_ds.clat, pres_ds.pres.isel(time=0, height=-1))
+    plt.tripcolor(lon, lat, pres_ds.pres.isel(time=0, height=-1))
     plt.show()
 else:
     print("Not creating plot of region.")
@@ -59,14 +85,25 @@ else:
 while lev >= lev_start :
     print( 'Finding center on lev:', lev)
 
-    single_lev = pres_ds.pres.isel(height=lev-1)
     # 1. First guess:
-    clon_fg = single_lev.where(single_lev == single_lev.min(), \
+    if coord_unit=='rad':
+        single_lev = pres_ds.pres.isel(time = 0, height=lev-1)
+        clon_fg = single_lev.where(single_lev == single_lev.min(), \
               drop=True).clon.values[0]
-    clat_fg = single_lev.where(single_lev == single_lev.min(), \
+        clat_fg = single_lev.where(single_lev == single_lev.min(), \
               drop=True).clat.values[0]
 
-    center[lev-lev_start,] = get_clonlat([clon_fg, clat_fg], single_lev, p_env[lev-lev_start], r_rad)
+        center[lev-lev_start,] = get_clonlat([clon_fg, clat_fg], single_lev, p_env[lev-lev_start], r_rad)
+    
+    if coord_unit=='deg':
+        single_lev = pres_ds.isel(time = 0, height=lev-1)
+        lon_fg = single_lev.where(single_lev == single_lev.min(), \
+              drop=True).lon.values[0]
+        lat_fg = single_lev.where(single_lev == single_lev.min(), \
+              drop=True).lat.values[0]
+ 
+        center[lev-lev_start,] = get_lonlat([lon_fg, lat_fg], single_lev, p_env[lev-lev_start], r_deg)
+
     lev -= 1
     print(center)
 
---
+++ IdealizeCyclone/Utilities/find_lonlat.py
@@ -0,0 +1,77 @@
+import numpy as np
+import math
+import xarray as xr
+from numba import jit
+
+@jit(parallel=True)
+def get_lonlat(fg, single_lev, p_env, r):
+    '''
+    Get the location of the center point on a single level, given a first guess and a data set.
+    r is radius of circle that should be selected.
+    '''
+   
+    # Extract first guess lon and lat
+    lon_fg = fg[0]
+    lat_fg = fg[1]
+   
+    # Cut out quadratic box containing all cells in possible circle region
+    lon_lim_east = lon_fg + r
+    lon_lim_west = lon_fg - r
+    lat_lim_north= lat_fg + r
+    lat_lim_south= lat_fg - r
+
+    # extract box containing all possible cells
+    single_lev = single_lev.where(single_lev['lon'] < lon_lim_east, drop=True)
+    single_lev = single_lev.where(single_lev['lon'] > lon_lim_west, drop=True)
+    single_lev = single_lev.where(single_lev['lat'] < lat_lim_north, drop=True)
+    single_lev = single_lev.where(single_lev['lat'] > lat_lim_south, drop=True)
+   
+    # Get radial distance of each cell to first guess.
+    # Following version of distance calculation gave wrong values:
+    #dist_test = scipy.spatial.distance.cdist(np.asarray([single_lev.clon.values, single_lev.clat.values]).reshape((len(lon_dist),2)), np.asarray([clon_fg, clat_fg]).reshape((1,2))) 
+    lon_array = np.empty([len(single_lev.lon)*len(single_lev.lat)]) 
+    lat_array = np.empty([len(single_lev.lon)*len(single_lev.lat)])
+    pres_array = np.empty([len(single_lev.lon)*len(single_lev.lat)])
+    dist_array = np.empty([len(single_lev.lon)*len(single_lev.lat)])
+    c = 0
+
+    for lon_i in range(len(single_lev.lon)):
+        for lat_i in range(len(single_lev.lat)):  
+            dist_array[c]= math.sqrt((single_lev.lon.values[lon_i] -lon_fg)**2 \
+                  + (single_lev.lat.values[lat_i] - lat_fg)**2)
+            lon_array[c] = single_lev.lon.values[lon_i]
+            lat_array[c] = single_lev.lat.values[lat_i]
+            pres_array[c] = single_lev.pres.values[lat_i, lon_i]
+            c =+ 1
+
+
+   
+    # Create new dataset combining pres,lon,lat and dist data.
+    circle_sel = xr.Dataset({'pres': pres_array, 'dist': dist_array, 'lon': lon_array, 'lat': lat_array})
+    # Extract region in circle
+    circle_sel = circle_sel.where(circle_sel['dist'] <= r, drop=True)
+
+    # Calculate average over all cells in circle region
+    #p_env = np.mean(circle_sel.pres.values)
+    # I am using p_max because otherwise I end up with lots of negative values and therefore get wrong values
+    #p_max = circle_sel.pres.values.max()
+    p_prime = p_env - circle_sel.pres.values
+
+    lon_new = np.sum(circle_sel.lon.values * p_prime)/np.sum(p_prime)
+    lat_new = np.sum(circle_sel.lat.values * p_prime)/np.sum(p_prime)
+
+    lon_new = circle_sel.lon.values[np.argmin(np.abs(circle_sel.lon.values \
+              - lon_new))]
+    lat_new = circle_sel.lat.values[np.argmin(np.abs(circle_sel.lat.values \
+              - lat_new))]
+   
+    if lat_fg != lat_new or lon_fg != lon_new:
+        # Recursively find the center
+        print('starting new calculation for center')
+        print(lon_new, lat_new)
+        get_lonlat([lon_new, lat_new], single_lev, p_env, r)
+    else:
+        print('Found: ', lon_new, lat_new) 
+        return (lon_new, lat_new)
+
+    return (lon_new, lat_new)
